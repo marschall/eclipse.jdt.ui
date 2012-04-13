b6cacbf822f214ed5701615e7bd3b40c818cd1d5
Michael Pellaton <michael.pellaton@gmail.com>
2012-04-13 12:22:23 +0200

    updated file header

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/wizards/NewPackageWizardPage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/wizards/NewPackageWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Philippe Marschall - package-info support (Bug-86168)
+ *     Michael Pellaton - package-info support (Bug-86168)
  *******************************************************************************/
 package org.eclipse.jdt.ui.wizards;
 
